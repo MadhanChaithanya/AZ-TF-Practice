176a229522a137179ee89c062a4554524cdce0de
MadhanChaithanya <39264182+MadhanChaithanya@users.noreply.github.com>
2023-03-01 22:59:20 +0530

    updated location

--- az-rg.tf
+++ az-rg.tf
@@ -16,6 +16,6 @@ provider "azurerm" {
 
 # Create the Azure Resource Group
 resource "azurerm_resource_group" "my_demo_rg1" {
-  location = "india"
+  location = "southindia"
   name = "my_demo_rg1"
 }
